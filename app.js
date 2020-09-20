8cb7f457f0440f2ba39f4de85f2f994e71441b11
Roman <romasherbina@gmail.com>
2020-09-20 16:46:26 +0300

    last update

--- app.js
+++ app.js
@@ -12,3 +12,57 @@ mobileMenu.addEventListener("click",function(){
    }
 
 })
+
+
+
+
+let autorization= document.querySelector(".autorization");
+let closeBtn=document.querySelector(".btn-close")
+
+// moredetails.forEach(function(btn){
+//     btn.addEventListener("click",OpenModal)
+//     })
+    
+    function OpenModal(){
+        //  e.preventDefault();
+        autorization.classList.add("show");
+        autorization.classList.remove("hide")
+    }
+
+    function OpenModalSend(){
+        //  e.preventDefault();
+        autorization.classList.add("show");
+        autorization.classList.remove("hide")
+    }
+
+    
+    function closeModal(){
+        autorization.classList.add("hide")
+        autorization.classList.remove("show")
+    }
+    closeBtn.addEventListener("click",closeModal)
+    
+    autorization.addEventListener("click",function(e){
+       if(e.target==autorization){
+          closeModal();
+       }
+    })
+    
+
+    function showModalByscroll(){
+        if(window.pageYOffset > document.documentElement.scrollHeight/2){
+         
+           OpenModal()
+           removeEventListener("scroll",showModalByscroll)
+         
+        }
+     }
+     
+     window.addEventListener("scroll",showModalByscroll)
+    
+     let openmodalbtn= document.querySelector(".openmodal button");
+     console.log(openmodalbtn);
+
+     openmodalbtn.addEventListener("click",OpenModal);
+
+     
